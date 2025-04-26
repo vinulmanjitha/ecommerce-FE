2137aaaa994ef6b6caf99e2fdb551ec1cef0b83d
vinulmanjitha <vinulmanjitha98@gmail.com>
2025-04-26 20:50:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavbarComponent from './components/Navbar/NavbarComponent';
+
+const App = () => {
+  return (
+    <div className="app">
+      <NavbarComponent />
+    </div>
+  );
+}
+
+export default App;
